444d28935e4eef53cd01fd5a81c5634fc0f2a0b2
artichoke-ci <ci@artichokeruby.org>
2020-12-19 17:46:03 +0000

    deploy: eb36dd48e16626457a46a487b67e51226ebd8f79

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["getrandom"] = [{"text":"impl StructuralEq for Error","synthetic":false,"types":[]}];
-implementors["rand_mt"] = [{"text":"impl StructuralEq for RecoverRngError","synthetic":false,"types":[]}];
+implementors["rand_mt"] = [{"text":"impl StructuralEq for Mt19937GenRand32","synthetic":false,"types":[]},{"text":"impl StructuralEq for Mt19937GenRand64","synthetic":false,"types":[]},{"text":"impl StructuralEq for RecoverRngError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
